ae65f44d69c0b8f0255780b7b9da8c1c8d41c13d
Basar Yuksel <basyuksel.app.development@gmail.com>
2017-09-09 22:17:11 +0300

    Commit by commit script automatically. Do not worry about the contents. Probably a semi colon fix or a curly bracket delete here and there.

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ const app = express();
 // DEFINE ALL API ROUTES RIGHT HERE, BEFORE SWITCH!!
 // //////////////////////////////////////////////////
 
-app.get('server.js', (req, res) => { /* do somewhat different instead of sending server.js to client */ }
-
 /* eslint-disable no-case-declarations global-require */
 switch (true) {
   case process.env.NODE_ENV !== 'production':
