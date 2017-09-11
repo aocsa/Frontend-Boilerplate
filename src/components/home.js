d9a707ca95a570c8a932cbb106e39297d651d7a0
Basar Yuksel <basyuksel.app.development@gmail.com>
2017-09-11 12:00:01 +0300

    Tried to exlude the 250kb babel-polyfill from bundle, but I couldnt make the babel plugins required to do so to work.

--- src/components/home.js
+++ src/components/home.js
@@ -3,10 +3,17 @@ import { connect } from 'react-redux';
 
 class Home extends Component {
   render() {
-    return <div>This is the Home component. Your boilerplate works!</div>;
+    const test = async () => {
+      await setTimeout(() => {
+        console.log('works');
+      }, 10);
+    };
+    test();
+    return <div>This is the Home component. Your boilerplate works </div>;
   }
 }
 
+// test
 export default connect()(Home);
 
 // The '/' route as seen on page load. Did not make it a pure functional component because you will need to add stuff here.
